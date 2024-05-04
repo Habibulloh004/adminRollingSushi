9df80b40bd750a9216c5eba73c855855b0771d97
habibulloh004 <habibulloh4080@gmail.com>
2024-05-04 14:57:27 +0500

    green icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,13 +13,18 @@ const Map = (prop) => {
       "https://as1.ftcdn.net/v2/jpg/07/00/28/16/1000_F_700281654_r9dsHGGMdHRuqshtB5DQ4qOSzQzOamqW.webp",
     iconSize: [30, 30],
   });
+  const icon2 = new Icon({
+    iconUrl:
+      "https://cdn1.iconfinder.com/data/icons/basic-ui-elements-coloricon/21/06_1-512.png",
+    iconSize: [30, 30],
+  });
 
   return (
     <>
       <div className="w-[100%] h-[350px]">
         <MapContainer
           center={[lat, lng]}
-          zoom={12}
+          zoom={11}
           style={{ width: "100%", height: "100%" }}
         >
           <TileLayer
@@ -31,22 +36,17 @@ const Map = (prop) => {
               <h3>hello I'm client</h3>
             </Popup>
           </Marker>
-          <Marker position={[41.267193, 69.226858]} icon={icon}>
+          <Marker position={[41.267193, 69.226858]} icon={icon2}>
             <Popup>
               <h3>Yakkasaroy</h3>
             </Popup>
           </Marker>
-          <Marker position={[41.267193, 69.226858]} icon={icon}>
-            <Popup>
-              <h3>Yakkasaroy</h3>
-            </Popup>
-          </Marker>
-          <Marker position={[41.350852, 69.244140]} icon={icon}>
+          <Marker position={[41.350852, 69.244140]} icon={icon2}>
             <Popup>
               <h3>Olmazor</h3>
             </Popup>
           </Marker>
-          <Marker position={[41.318682, 69.339927]} icon={icon}>
+          <Marker position={[41.318682, 69.339927]} icon={icon2}>
             <Popup>
               <h3>Buyuk ipak yo'li</h3>
             </Popup>
